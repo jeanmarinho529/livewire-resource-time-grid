88dab7c648086b2f2de96a308f1265c935810d87
jeanmarinho529 <jeanmarinho529@gmail.com>
2026-04-17 16:54:01 -0300

    up

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -3,8 +3,8 @@
     class="{{ $styles['event'] }}"
     wire:click.stop="onEventClick('{{ $event['id'] }}')">
 
-    <div class="{{ $styles['eventTitle'] }}">
-        {{ $event['starts_at']->format('H:i') }} - {{ $event['ends_at']->format('H:i') }}
+    <div class="{{ $styles['eventTitle'] }} {{ $event['color'] }}">
+                {{ $event['starts_at']->format('H:i') }} - {{ $event['ends_at']->format('H:i') }} - {{ $event['status'] }}
     </div>
 
     <div class="{{ $styles['eventBody'] }}">
